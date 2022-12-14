b97e69b4545022216c2d08da9b04bc507a059f84
Robson Messias <dev.robsonmessias@gmail.com>
2022-12-14 17:00:41 -0300

    delete calc example

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,24 +1,4 @@
 const expect = require('chai').expect;
+const calc = require('../src/main');
 
-describe('Main', function () {
-    let arr = [];
 
-    beforeEach(function () {
-        arr = [1, 2, 3];
-    });
-
-    it('should have a size of 4 when push another value to the array', function () {
-        arr.push(4);
-        expect(arr).to.have.lengthOf(4);
-    });
-
-    it('should have a size of 2 when pop a value from the array', function () {
-        arr.pop();
-        expect(arr).to.have.lengthOf(2);
-    });
-
-    it('should remove the value 3 when use pop in the array', function () {
-        arr.pop()
-        expect(arr).to.not.include(3);
-    });
-});
